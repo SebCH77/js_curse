a13b843994badb5e947002b819633304e3ca8d2d
Sebastian Carrillo <scarrillo@lya-electronic.com>
2022-04-13 17:06:02 -0500

    Clousures

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -325,25 +325,111 @@ document
 
 ///////////////////////////////////////
 // Immediately Invoked Function Expressions (IIFE)
-const runOnce = function () {
-  console.log('This will never run again');
-};
-runOnce();
+// const runOnce = function () {
+//   console.log('This will never run again');
+// };
+// runOnce();
 
 // IIFE
-(function () {
-  console.log('This will never run again');
-  const isPrivate = 23; //Todos los datos definidos dentro de un alcance son privados. (Datos encapsulados).
-})();
+// (function () {
+//   console.log('This will never run again');
+//   const isPrivate = 23; //Todos los datos definidos dentro de un alcance son privados. (Datos encapsulados).
+// })();
 
 // console.log(isPrivate);
 
-(() => console.log('This will ALSO never run again'))();
+// (() => console.log('This will ALSO never run again'))();
 
 //Bloque:
-{
-  const isPrivate = 23; //No es accesible por fuera.
-  var notPrivate = 46; //Sera accesible por fuera del bloque, ya que es global.
-}
+// {
+//   const isPrivate = 23; //No es accesible por fuera.
+//   var notPrivate = 46; //Sera accesible por fuera del bloque, ya que es global.
+// }
 // console.log(isPrivate);
-console.log(notPrivate);
+// console.log(notPrivate);
+
+///////////////////////////////////////
+// Closures: NO se escriben explicitamente. Solo ocurren en ciertas situaciones de manera IMPLICITA.
+// const secureBooking = function () {
+//   let passengerCount = 0;
+
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+//Lo anterior queda INACTIVO, Ya que:
+/* const booker = secureBooking(); */ //Es asi que booker tiene acceso a las variables ubicadas en el environmment de secureBooking!
+
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
+
+///////////////////////////////////////
+// More Closure Examples
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+// const boardPassengers = function (n, wait) {
+//   const perGroup = n / 3;
+
+//   setTimeout(function () {
+//     console.log(`We are now boarding all ${n} passengers`);
+//     console.log(`There are 3 groups, each with ${perGroup} passengers`);
+//   }, wait * 1000);
+
+//   console.log(`Will start boarding in ${wait} seconds`);
+// };
+
+// const perGroup = 1000;
+// boardPassengers(180, 3);
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+/*
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+*/
