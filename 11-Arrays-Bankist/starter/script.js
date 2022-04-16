9c0f3e9effd97881f4213278de831aad9024f941
Sebastian Carrillo <scarrillo@lya-electronic.com>
2022-04-16 01:35:38 -0500

    Creating DOM Elements

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -62,6 +62,31 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
+// Functions
+
+const displayMovements = function (movements, sort = false) {
+  containerMovements.innerHTML = ''; //Para vaciar el contenedor.
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}€</div>
+      </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html); //Primer param: posición en la que quiero adjuntar el html.
+  });
+};
+displayMovements(account1.movements);
+
+/////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
